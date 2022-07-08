6604051efe655ee2b8383ab6d4593c3e5aa73c49
Алина Прескура <a.preskura@logic-way.local>
2022-07-08 16:55:28 +0300

    added task19

---
+++ seminar3/task19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+namespace task19 {
+    class Program {
+        static void Main(string[] args) {
+            int numberN = Convert.ToInt32(Console.ReadLine());                         // Ввод числа:
+
+            if (numberN < 10000 || numberN > 99999) {                                  // Первая скорая проверка на достоверность ввода
+                Console.WriteLine("This nomber is not polindrom length five");
+            } else if (numberN % 10 == numberN / 10000 & (numberN / 10) % 10 == (numberN / 1000) % 10) {
+                Console.WriteLine("Yes");
+            } else {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
